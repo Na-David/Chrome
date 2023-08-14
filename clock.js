a548607cf80913ce779e411293fc934982214fd6
David Na <hna206@naver.com>
2023-08-15 00:56:05 +1200

    Time setting

--- clock.js
+++ clock.js
@@ -1,2 +1,16 @@
 const dateElement = document.getElementById("date");
 const timeElement = document.getElementById("time");
+
+const getNowDate = () => {
+    const nowDate = new Date();
+
+    const week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    let month = nowDate.getMonth() + 1;
+    let date = nowDate.getDate();
+    let day = nowDate.getDay();
+
+    console.log(month, date, day);
+}
+
+getNowDate();
