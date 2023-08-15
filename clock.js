70974be79d21736b3f42de704ef325da91335bb7
David Na <hna206@naver.com>
2023-08-16 00:12:04 +1200

    added monthList && modified week

--- clock.js
+++ clock.js
@@ -1,16 +1,32 @@
 const dateElement = document.getElementById("date");
 const timeElement = document.getElementById("time");
 
+const modifyNumber = (month) => {
+
+    // if( month < 10){
+    //     return "0"+ month
+    // } else {
+    //     return month
+    // }
+
+    return parseInt(month) < 10 ? "0" + month : month;
+}
+
 const getNowDate = () => {
     const nowDate = new Date();
 
-    const week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const week = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+    const monthList = ["January","February","March","April","May","Jun","July","August","September","October","November","December"];
 
-    let month = nowDate.getMonth() + 1;
-    let date = nowDate.getDate();
-    let day = nowDate.getDay();
+    let month = monthList[nowDate.getMonth()];
+    let date = modifyNumber(nowDate.getDate());
+    let day = week[nowDate.getDay()];
 
     console.log(month, date, day);
+}
+
+const setNowDate = (month, date, day) => {
+    dateElement.textContent = `${month}`
 }
 
 getNowDate();
