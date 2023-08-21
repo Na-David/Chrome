b82924c24938efe30bf757b8dbc66a8d4adc8e5c
David Na <hna206@naver.com>
2023-08-21 20:39:49 +1200

    branch for - bible verse

--- bible.js
+++ bible.js
@@ -1,23 +1,23 @@
 const API_KEY = "9FnZ0tl0VxGKRXh5vowvA14NCL6enI9TDswn6wKx";
 
-
-const bibleElement = document.getElementById("bible");
-
-const getBible = async() => {
-    
-    const API_ENDPOINT = "http://quotes.rest/bible/vod.js";
+function fetchRandomVerse() {
+    const API_ENDPOINT = "http://quotes.rest/bible/verse.json";
     const headers = {
         "Authorization": `Bearer ${API_KEY}`
     };
 
-    const data = await fetch(API_ENDPOINT, { headers }).then((res) => res.json)
-
-    console.log(data);
+    fetch(API_ENDPOINT, { headers })
+        .then(response => response.json())
+        .then(data => {
+            const verse = data.contents.quote;  // API 응답에 따라 경로가 다를 수 있습니다.
+            displayVerse(verse);
+        })
+        .catch(error => console.error("Error fetching the Bible verse:", error));
 }
 
-const displayBible = (data) => {
-    const bible = document.getElementById("bible");
-    bible.textContent = data + bible.textContent;
+function displayVerse(verse) {
+    const bibleDiv = document.getElementById("bible");
+    bibleDiv.innerHTML = verse + bibleDiv.innerHTML;  // 성경 구절을 div에 추가합니다.
 }
 
-getBible();
+document.addEventListener("DOMContentLoaded", fetchRandomVerse);
