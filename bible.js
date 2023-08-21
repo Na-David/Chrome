dd349a89b2ffa5f68b8496d1c8ae001b243cddd6
David Na <hna206@naver.com>
2023-08-21 21:52:34 +1200

    proverbs

--- bible.js
+++ bible.js
@@ -1,15 +1,41 @@
 const API_URL = "https://random-quote.hyobb.com"
 const quoteElement =  document.getElementById("bible");
 
+const quoteItem = localStorage.getItem("quote");
+
+const nowDate = new Date();
+const month = nowDate.getMonth() + 1;
+const date = nowDate.getDate();
+
+const setQuote = (result) => {
+    let quote = {createDate : `${month}-${date}` , quoteData : result};
+    localStorage.setItem("quote", JSON.stringify(quote));
+    quoteElement.textContent = result;
+}
+
 const getQuote = async() => {
     try {
         const data = await fetch(API_URL).then((res) => res.json());
-        console.log(data);
+
         const result = data[1].respond;
-        console.log(result);   
+        setQuote(result);
+
     } catch(err) {
         console.log(`err: ${err}`);
+        setQuote("Now or Never.")
     }
 }
+
+if (quoteItem) {
+    let {createDate, quoteData} = JSON.parse(quoteItem)
+    if ( createDate === `${month}-${date}`){
+        quoteElement.textContent = `"${quoteData}"`;
+    } else {
+        getQuote();
+    }
+} else {
+    getQuote();
+}
+
 
 getQuote();
